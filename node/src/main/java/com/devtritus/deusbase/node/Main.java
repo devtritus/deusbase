1a7b48a1cd46d9bb69874ce8b670d83e348ae576
devtritus <i.lutsenko@outlook.com>
2020-05-08 17:50:07 +0300

    move initialization of actors loader to actors loader

--- node/src/main/java/com/devtritus/deusbase/node/Main.java
+++ node/src/main/java/com/devtritus/deusbase/node/Main.java
@@ -5,21 +5,13 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import com.devtritus.deusbase.api.ProgramArgs;
 import com.devtritus.deusbase.api.ProgramArgsParser;
-import com.devtritus.deusbase.node.env.NodeEnvironment;
-import com.devtritus.deusbase.node.server.*;
-import com.devtritus.deusbase.node.storage.ValueStorage;
-import com.devtritus.deusbase.node.tree.BTree;
-import com.devtritus.deusbase.node.tree.BTreeInitializer;
 import com.devtritus.deusbase.node.utils.ActorsLoader;
 import com.devtritus.deusbase.node.utils.NodeMode;
-
-import java.util.List;
 
 import static com.devtritus.deusbase.api.ProgramArgNames.*;
 import static com.devtritus.deusbase.node.env.NodeSettings.*;
 
 public class Main {
-
     static {
         Logger root = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
         root.setLevel(Level.INFO);
@@ -30,19 +22,7 @@ public class Main {
 
         NodeMode mode = NodeMode.fromText(programArgs.getOrDefault(MODE, DEFAULT_NODE_MODE));
         if(mode == NodeMode.LOAD_DATA) {
-            NodeEnvironment env = new NodeEnvironment();
-            env.setUp(programArgs);
-
-            int treeM = programArgs.getIntegerOrDefault(TREE_M, DEFAULT_TREE_M);
-            int treeCacheLimit = programArgs.getIntegerOrDefault(TREE_CACHE_LIMIIT, DEFAULT_TREE_CACHE_LIMIT);
-
-            BTree<String, List<Long>> tree = BTreeInitializer.init(env.getIndexPath(), treeM, treeCacheLimit);
-            ValueStorage storage = new ValueStorage(env.getStoragePath());
-
-            NodeApi nodeApi = new NodeApi(tree, storage);
-            CrudRequestHandler requestHandler = new CrudRequestHandler();
-            requestHandler.setApi(nodeApi);
-            ActorsLoader.load(programArgs, requestHandler);
+            ActorsLoader.load(programArgs);
         } else {
             new Node(mode, programArgs).start();
         }
